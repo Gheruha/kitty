6cc7653b4d68f43dec4c128fd57eccfb89c55c0d
Gheruha <maxgheruha@gmail.com>
2025-07-22 15:42:47 +0300

    Made first executable kitten

---
+++ kittens/changeTheme/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+from kitty.boss import Boss
+def main(args: list[str]) -> str:
+    answer = input("Enter some text: ")
+
+    # whatever this function returns will be available inside handle_result
+    return answer
+
+def handle_result(args: list[str], answer: str, target_window_id: int, boss: Boss) -> None:
+    # get the kitty window into which to paste the text
+    w = boss.window_id_map.get(target_window_id)
+    if w is not None:
+        w.paste_text(answer)
